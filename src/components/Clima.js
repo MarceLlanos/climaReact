160cb23129d0e68031ccfcea0886c1552788c1b9
Marcela Llanos <marcela3llanos@gmail.com>
2019-09-15 16:49:58 -0400

    Se agrego el card completo que muestra todos los datos del clima segun la ciudad

--- src/components/Clima.js
+++ src/components/Clima.js
@@ -2,11 +2,37 @@ import React, { Component } from 'react';
 
 class Clima extends Component {
   
+  mostrarResultado = () => {
+    //Obtener los datos de la consulta
+    const {name, weather, main} = this.props.resultado;
+    if(!name || !weather || !main){
+      return null;
+    }
+    const kelvin = 273.15;
+    const urlIcon = `http://openweathermap.org/img/wn/${weather[0].icon}.png`;
+    return(
+      <div className = "row">
+        <div className = "resultado col s12 m8 l6 offset-m2 offset-13">
+          <div className = "card-panel light-blue align-center">
+            <span className = "white-text">
+              <h2>Resultado Clima de: {name}</h2>
+              <p className = "temperatura">
+                Actual: {(main.temp - kelvin).toFixed(2)} &deg;C
+                <img src = {urlIcon} alt = "`${name}`"/>
+              </p>
+              <p>Max. {main.temp_max - kelvin} &deg;C</p>
+              <p>Min. {main.temp_min - kelvin} &deg;C</p>
+            </span>
+          </div>
+        </div>
+      </div>
+    )
+  }
   render() { 
     
     return ( 
       <div className = "container">
-        <h1>Desde clima.js</h1>
+        {this.mostrarResultado()}
       </div>
      );
   }
